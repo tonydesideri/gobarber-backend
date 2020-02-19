bb786c31d15f91e5a1805841d986fad054a94dee
Tony Desideri <tony_desideri@hotmail.com>
2020-02-18 21:41:29 -0400

    relacionamento user-file

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
-import SessionControle from './app/controllers/SessionController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -11,14 +12,12 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
-routes.post('/sessions', SessionControle.store);
+routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
