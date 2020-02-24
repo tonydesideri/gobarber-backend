532663d5bbb3ab1a55d05d330785a09d21f5b6b2
Tony Desideri <tony_desideri@hotmail.com>
2020-02-24 18:46:45 -0400

    avalible date

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleCoctroler';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AveilableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/provides/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
